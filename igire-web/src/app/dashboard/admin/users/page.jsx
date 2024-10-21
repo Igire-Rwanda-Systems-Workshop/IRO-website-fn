f8528b78fa38fd17a40f298c0cd6a27766ad75f1
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-10-21 16:10:48 +0200

    fetching all users

--- igire-web/src/app/dashboard/admin/users/page.jsx
+++ igire-web/src/app/dashboard/admin/users/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { TbPencil } from "react-icons/tb";
@@ -11,13 +11,15 @@ import DeleteUserModal from './deleteUserModal';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { HiOutlineSearch } from "react-icons/hi";
+import { FaSpinner } from 'react-icons/fa';
+import axios from 'axios';
 
-const UserCard = ({ name, email, jobTitle, onEdit, onDelete }) => {
+const UserCard = ({ id, name, email, role, onEdit, onDelete }) => {
   return (
-    <Card className="w-full sm:w-[calc(33.333%-0.75rem)] shadow-sm relative">
+    <Card className="w-full shadow-sm relative z-1">
       <div className="absolute top-2 right-2 flex flex-col space-y-2">
         <TbPencil className="text-[#0FA958] cursor-pointer" onClick={onEdit} />
-        <RiDeleteBin6Line className="text-[#FF0202] cursor-pointer" onClick={onDelete} />
+        <RiDeleteBin6Line className="text-[#FF0202] cursor-pointer" onClick={() => onDelete(id)} />
       </div>
       <CardContent className="flex items-center justify-center bg-[#EAEAEA] rounded-lg">
         <div className="flex flex-col items-center pt-6 gap-4">
@@ -25,7 +27,7 @@ const UserCard = ({ name, email, jobTitle, onEdit, onDelete }) => {
           <div className="text-center">
             <CardTitle className="text-lg font-normal">{name}</CardTitle>
             <p className="text-sm text-muted-foreground">{email}</p>
-            <p className="text-sm text-muted-foreground">{jobTitle}</p>
+            <p className="text-sm text-muted-foreground">{role}</p>
           </div>
         </div>
       </CardContent>
@@ -37,61 +39,72 @@ const ManageUsers = () => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://iro-website-bn-vx04.onrender.com/api/inventory/users/allUsers');
+        console.log("API Response:", response.data);
+        setUsers(response.data.users || response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        setError("Failed to fetch users.");
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <>
       <div className='p-2 sm:p-8'>
         <div>
-          <div>
-            <p className='text-[18px] sm:items-center gap-3 flex flex-col sm:flex-row items-start justify-between'>
-              Manage User
-              <span>
-                <div className="relative sm:w-80 w-full ">
-                  <Input
-                    placeholder="Search user..."
-                    // value={deniedGlobalFilter}
-                    // onChange={(event) => setDeniedGlobalFilter(event.target.value)}
-                    className="pl-10"
-                  />
-                  <HiOutlineSearch className="absolute left-3 top-1/2 transform -translate-y-1/2" />
-                </div>
-              </span>
-              <span
-                className='flex'
-                onClick={() => setIsAddModalOpen(true)}
-              >
-                <Button className="flex gap-2 text-white" >
-                  add user
-                  <IoIosAddCircleOutline />
-                </Button>
-              </span>
-            </p>
+          <div className='text-[18px] sm:items-center gap-3 flex flex-col sm:flex-row items-start justify-between'>
+            <p>Manage User</p>
+            <div className="relative sm:w-80 w-full">
+              <Input
+                placeholder="Search user..."
+                className="pl-10"
+              />
+              <HiOutlineSearch className="absolute left-3 top-1/2 transform -translate-y-1/2" />
+            </div>
+            <span
+              className='flex'
+              onClick={() => setIsAddModalOpen(true)}
+            >
+              <Button className="flex gap-2 text-white">
+                add user
+                <IoIosAddCircleOutline />
+              </Button>
+            </span>
           </div>
         </div>
-
-        <div className="flex flex-col sm:flex-row gap-[2rem] w-full pt-[2.7rem] z-0">
-          <UserCard
-            name="Hirwa J. Eric"
-            email="useremail@gmail.com"
-            jobTitle="Operation Manager"
-            onEdit={() => setIsEditModalOpen(true)}
-            onDelete={() => setIsDeleteModalOpen(true)}
-          />
-          <UserCard
-            name="Hirwa J. Eric"
-            email="useremail@gmail.com"
-            jobTitle="Operation Manager"
-            onEdit={() => setIsEditModalOpen(true)}
-            onDelete={() => setIsDeleteModalOpen(true)}
-          />
-          <UserCard
-            name="Hirwa J. Eric"
-            email="useremail@gmail.com"
-            jobTitle="Operation Manager"
-            onEdit={() => setIsEditModalOpen(true)}
-            onDelete={() => setIsDeleteModalOpen(true)}
-          />
-        </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-48">
+            <FaSpinner className="animate-spin text-3xl" />
+          </div>
+        ) : (
+          <div className="flex flex-col flex-wrap sm:flex-row gap-[2rem] w-full pt-[2.7rem] z-0">
+            {users.map((user, index) => (
+              <div className="w-full sm:w-[calc(33.333%-1.5rem)]" key={index}>
+                <UserCard
+                 id={user.id}
+                  name={user.name}
+                  email={user.email}
+                  role={user.role}
+                  onEdit={() => setIsEditModalOpen(true)}
+                  onDelete={() => setIsDeleteModalOpen(true)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
       <AddUserModal isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)} />
--- igire-web/src/app/dashboard/hr/(components)/DashboardSideBar.jsx
+++ igire-web/src/app/dashboard/hr/(components)/DashboardSideBar.jsx
@@ -15,10 +15,10 @@ export default function DashboardSideBar() {
 
     return (
         <div className="desktop:block hidden h-full">
-            <div className="sticky top-0 flex h-full max-h-screen flex-col gap-[9rem] pt-10 
-                text-white bg-[#0B3004] border-r">
-                <div className="flex h-[55px] items-center justify-between px-3 w-full">
-                    <div className="flex items-center">
+            <div className="sticky top-0 flex h-full max-h-screen flex-col gap-[9rem] py-10 
+                text-white bg-[#0B3004] border-r  pl-8">
+                <nav className="flex flex-1 justify-between flex-col items-start text-md">
+                    <div className="flex items-center w-full">
                         <Image
                             src="/logo.png"
                             alt="LOGO"
@@ -30,65 +30,62 @@ export default function DashboardSideBar() {
                             Igire Rwanda <span className="font-normal">Organization</span>
                         </p>
                     </div>
-                </div>
-                <div className="flex-1 overflow-auto py-2">
-                    <nav className="grid items-start pl-8 text-md gap-[10rem]">
-                        <div>
-                            <Link
-                                href="/dashboard/hr"
-                                className={clsx(
-                                    "flex items-center gap-2 rounded-lg px-3 py-2 transition-all",
-                                    pathname === '/dashboard/hr' && "text-[#F79E1B]"
-                                )}
-                            >
-                                <GoHome className="h-3 w-3" />
-                                Home
-                            </Link>
-                            <Link
-                                href="/dashboard/hr/applications"
-                                className={clsx(
-                                    "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
-                                   pathname.startsWith('/dashboard/hr/applications') && "text-[#F79E1B]"
-                                )}
-                            >
-                                <HiOutlineUsers className="h-3 w-3" />
-                                Applications
-                            </Link>
-                            <Link
-                                href="/dashboard/hr/appointments"
-                                className={clsx(
-                                    "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
-                                    pathname.startsWith('/dashboard/hr/appointments') && "text-[#F79E1B]"
-                                )}
-                            >
-                                <LiaTagsSolid className="h-3 w-3" />
-                                Appointments
-                            </Link>
-                            <Link
-                                href="/dashboard/hr/hiring"
-                                className={clsx(
-                                    "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
-                                    pathname.startsWith('/dashboard/hr/hiring') && "text-[#F79E1B]"
-                                )}
-                            >
-                                <HiOutlineArchiveBox className="h-3 w-3" />
-                                Initiate hiring
-                            </Link>
-                        </div>
-                        <div>
-                            <Link
-                                href="/dashboard/hr/logout"
-                                className={clsx(
-                                    "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
-                                    pathname === '/dashboard/hr/logout' && "text-[#F79E1B]"
-                                )}
-                            >
-                                <RiLogoutCircleLine className="h-3 w-3" />
-                                Logout
-                            </Link>
-                        </div>
-                    </nav>
-                </div>
+
+                    <div className="flex flex-col gap-5">
+                        <Link
+                            href="/dashboard/hr"
+                            className={clsx(
+                                "flex items-center gap-2 rounded-lg px-3 py-2 transition-all",
+                                pathname === '/dashboard/hr' && "text-[#F79E1B]"
+                            )}
+                        >
+                            <GoHome className="h-3 w-3" />
+                            Home
+                        </Link>
+                        <Link
+                            href="/dashboard/hr/applications"
+                            className={clsx(
+                                "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
+                                pathname.startsWith('/dashboard/hr/applications') && "text-[#F79E1B]"
+                            )}
+                        >
+                            <HiOutlineUsers className="h-3 w-3" />
+                            Applications
+                        </Link>
+                        <Link
+                            href="/dashboard/hr/appointments"
+                            className={clsx(
+                                "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
+                                pathname.startsWith('/dashboard/hr/appointments') && "text-[#F79E1B]"
+                            )}
+                        >
+                            <LiaTagsSolid className="h-3 w-3" />
+                            Appointments
+                        </Link>
+                        <Link
+                            href="/dashboard/hr/hiring"
+                            className={clsx(
+                                "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
+                                pathname.startsWith('/dashboard/hr/hiring') && "text-[#F79E1B]"
+                            )}
+                        >
+                            <HiOutlineArchiveBox className="h-3 w-3" />
+                            Initiate hiring
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            href="/dashboard/hr/logout"
+                            className={clsx(
+                                "flex items-center gap-2 rounded-lg px-3 py-2 transition-all hover:text-[#F79E1B]",
+                                pathname === '/dashboard/hr/logout' && "text-[#F79E1B]"
+                            )}
+                        >
+                            <RiLogoutCircleLine className="h-3 w-3" />
+                            Logout
+                        </Link>
+                    </div>
+                </nav>
             </div>
         </div>
     );
